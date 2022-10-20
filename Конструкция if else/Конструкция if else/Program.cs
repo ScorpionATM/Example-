a3ab00e53c9a97ebaed9996fe50cf2052997b019
Ahrorbek <tolashevahrorbek@gmail.com>
2022-10-20 06:14:27 -0700

    If else

---
+++ Конструкция if else/Конструкция if else/Program.cs
@@ -0,0 +1,22 @@
+﻿int num1 = 8;
+int num2 = 6;
+if (num1 > num2)
+{
+    Console.WriteLine($" {num1}  {num2}");
+}
+else
+{
+    Console.WriteLine($" {num2}   {num1}");
+}
+
+
+string name = "Alex";
+
+if (name == "Tom")
+    Console.WriteLine("Вас зовут Tomas");
+else if (name == "Bob")
+    Console.WriteLine("Вас зовут Robert");
+else if (name == "Mike")
+    Console.WriteLine("Вас зовут Michael");
+else
+    Console.WriteLine("Неизвестное имя");
