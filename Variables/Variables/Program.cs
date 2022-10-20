a32722701bf81a28946d2e9f0fdd73f90f4c460c
Ahrorbek <tolashevahrorbek@gmail.com>
2022-10-20 06:01:25 -0700

    Variable

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿string name = "Ahrorbek";
+const string NAME = "ATM";
+
+Console.WriteLine(name);
+
+int namber  = 10;
+
+object intnamber = 11;
+object floatnumber = 3.14;
+object string1 = "Hello world";
+
+//'\n' - перевод строки
+
+//'\t' - табуляция
+
+//'\\' - слеш
+
+Console.WriteLine(string1 + NAME);
+
+int a = 10;
+int b = 4;
+bool c = a > b;    
+bool d = a > 25;
+Console.WriteLine(c);
+Console.WriteLine(d);
