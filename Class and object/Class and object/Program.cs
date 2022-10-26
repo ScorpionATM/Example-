bd62fb0b9ab4ec20d49579fea326ab3cfe4189b9
Abdulaziz <97102064+BatchatPro@users.noreply.github.com>
2022-10-26 18:03:47 +0500

    Zadacha

---
+++ Class and object/Class and object/Program.cs
@@ -0,0 +1,13 @@
+﻿Person tom = new Person();  // создание объекта класса Person
+
+// определение класса Person
+class Person
+{
+    public string name = "Undefined";
+    public int age;
+
+    public void Print()
+    {
+        Console.WriteLine($"Имя: {name}  Возраст: {age}");
+    }
+}
---
+++ Задача 1/Задача 1/Program.cs
@@ -0,0 +1,147 @@
+﻿// 1.3
+//var x = Console.ReadLine();
+
+//Console.WriteLine($"Siz shu sonni kirittingiz: {x}");
+
+// 1.4
+//var x = Console.ReadLine();
+
+//Console.WriteLine($"{x} Bu siz kiritgan raqam!");
+
+// 1.5
+//int a = 1;
+//int b = 13;
+//int c = 49;
+//Console.WriteLine($"{a} {b} {c}");
+
+// 1.6
+//int a = 1;
+//int b = 13;
+//int c = 49;
+//Console.WriteLine($"{a}  {b}  {c}");
+
+// 1.7
+//var a = Console.ReadLine();
+//var b = Console.ReadLine();
+//var d = Console.ReadLine();
+//Console.WriteLine($"{a}  {b}  {d}"); 
+
+// 1.8
+
+
+// 1.9
+//int[] nums = { 50, 10 };
+//foreach(int num in nums)
+//{
+//    Console.WriteLine(num); 
+//}
+
+
+// 1.10
+//int[] nums = { 5, 10 , 21};
+//foreach (int num in nums)
+//{
+//    Console.WriteLine(num);
+//}
+
+// 1.11
+//int[] nums = new int[4];
+//for (int i = 0; i < nums.Length; i++)
+//    nums[i] = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine();
+//foreach (int num in nums)  
+//    Console.WriteLine(num); 
+
+// 1.12
+//int a = 5;
+//int b = 10;
+//Console.WriteLine($"{a} {b} \n7sm");
+
+// 1.13
+//int a = 5;
+//int b = 10;
+//Console.WriteLine($"2kg  \n{a} {b}");
+
+// A1.1
+//Console.WriteLine("Masofani SM da kiriting!");
+//int nums = Convert.ToInt32(Console.ReadLine());
+//int x = nums / 100;
+//Console.WriteLine(x);
+
+// A1.2
+//Console.WriteLine("Massani kiriting!");
+//int nums = Convert.ToInt32(Console.ReadLine());
+//int x = nums / 100;
+//Console.WriteLine(x);
+
+// A1.3
+//Console.WriteLine("Massani kiriting!");
+//int nums = Convert.ToInt32(Console.ReadLine());
+//int x = nums / 1000;
+//Console.WriteLine(x);
+
+// A1.4
+//Console.WriteLine("Masofani M da kiriting!");
+//int nums = Convert.ToInt32(Console.ReadLine());
+//int x = nums / 1000;
+//Console.WriteLine(x);
+
+// A1.5
+//int Day = 234;
+//int respons = Day / 7;
+//Console.WriteLine(respons);
+
+// A1.6
+//int Hours = DateTime.Now.Hour;
+//Console.WriteLine(Hours);
+
+
+//int Hours = DateTime.Now.Minute;
+//Console.WriteLine(Hours);
+
+
+//int Hours = DateTime.Now.Second;
+//Console.WriteLine(Hours);
+
+// A1.7
+//int h = 543;
+//int w = 130;
+//int k = 130;
+//int s = h / k;
+//Console.WriteLine(s);
+
+// A1.8
+//var date = new DateTime(2023, 1, 1);
+//if (date.DayOfWeek == DayOfWeek.Sunday)
+//{
+//    Console.WriteLine("0");
+//}
+//if (date.DayOfWeek == DayOfWeek.Monday)
+//{
+//    Console.WriteLine("1");
+//}
+//if (date.DayOfWeek == DayOfWeek.Tuesday)
+//{
+//    Console.WriteLine("2");
+//}
+//if (date.DayOfWeek == DayOfWeek.Wednesday)
+//{
+//    Console.WriteLine("3");
+//}
+//if (date.DayOfWeek == DayOfWeek.Thursday)
+//{
+//    Console.WriteLine("4");
+//}
+//if (date.DayOfWeek == DayOfWeek.Friday)
+//{
+//    Console.WriteLine("5");
+//}
+//if (date.DayOfWeek == DayOfWeek.Saturday)
+//{
+//    Console.WriteLine("6");
+//}
+
+// A1.9
+//int x = Convert.ToInt32(Console.ReadLine());
+//int n = x + 1;
+//Console.WriteLine(n);   
